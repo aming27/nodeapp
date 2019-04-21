b0a127a8bedac7abe2f084cb54e443e0a391945b
root <root@localhost.localdomain>
2019-04-21 22:32:54 +0200

    Add 0.0.0.0 address

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const http = require('http')
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('Hello World\n')
-}).listen(1337, '127.0.0.1')
+}).listen(1337, '0.0.0.0')
 
 console.log('Server running at http://127.0.0.1:1337/')
