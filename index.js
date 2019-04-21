c29c3d37b9217cba54ad82fa5d0e8c27b4bb7676
root <root@localhost.localdomain>
2019-04-21 22:40:10 +0200

    Change listen address

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ http.createServer(function (req, res) {
   res.end('Hello World\n')
 }).listen(1337, '0.0.0.0')
 
-console.log('Server running at http://127.0.0.1:1337/')
+console.log('Server running at http://0.0.0.0:1337/')
